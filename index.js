69d0ee9a329a3365b30aaa6c9cdbb9d0985eb656
fabian cornejo <36684138+fabygithub@users.noreply.github.com>
2024-02-05 12:45:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function a(){
 
 
 function b(){
-	 window.open("https://s1.elespanol.com/2023/12/07/corazon/estilo/815429088_238246449_1706x960.jpg", "iframe");
+	 window.open("https://fabygithub.github.io/peluqueria/balayage.html", "iframe");
 }
 
 
