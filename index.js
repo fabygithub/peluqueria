179014fff6973fe20e5aafc45d15d2c1099fb348
fabian cornejo <36684138+fabygithub@users.noreply.github.com>
2024-02-05 14:53:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,3 +15,8 @@ function c(){
 	 window.open("https://fabygithub.github.io/peluqueria/noticias.html", "iframe");
 }
 
+function d(){
+	 window.open("https://media.istockphoto.com/id/1326014422/es/v%C3%ADdeo/im%C3%A1genes-de-video-4k-de-una-atractiva-mujer-joven-que-se-seca-el-cabello-en-un-sal%C3%B3n-durante.mp4?s=mp4-640x640-is&k=20&c=tdTX-vz4qq12lLE_TityM5-hmVb82qyeSoKRUnAqlEs=", "iframe");
+}
+
+
