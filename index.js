d443ff94006e243fd85b4333fcee6be67ab134e4
fabian cornejo <36684138+fabygithub@users.noreply.github.com>
2024-03-14 16:42:57 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function a(){
+    window.open("https://fabygithub.github.io/peluqueria/introduccion.html", "");
+}
+
+
+function a2(){
+    window.open("https://fabygithub.github.io/peluqueria/balayage.html", "");
+}
+
+function a3(){
+    window.open("https://fabygithub.github.io/peluqueria/trucos.html", "");
+}
+
+ 
